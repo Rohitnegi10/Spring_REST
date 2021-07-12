128cbb2a4a24a56c5cbc014488a1effe08e3987f
Rohit Negi <rohitnegicap10@gmail.com>
2021-07-12 20:14:41 +0530

    Practise Check

---
+++ Practise Check/Truyum/src/main/java/com/cognizant/truyum/exception/MenuItemException.java
@@ -0,0 +1,16 @@
+package com.cognizant.truyum.exception;
+
+public class MenuItemException extends RuntimeException{
+
+    public MenuItemException(String message) {
+        super(message);
+    }
+
+    public MenuItemException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MenuItemException(Throwable cause) {
+        super(cause);
+    }
+}
